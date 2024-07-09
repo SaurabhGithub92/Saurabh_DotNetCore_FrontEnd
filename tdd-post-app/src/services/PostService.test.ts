fb55378046d71097a8f86b669c7bd13a09d20ae8
saurabh <saurabh908@outlook.com>
2024-07-09 18:47:36 +0530

    PostService tests cases completed

--- tdd-post-app/src/services/PostService.test.ts
+++ tdd-post-app/src/services/PostService.test.ts
@@ -10,11 +10,19 @@ describe('PostService', () => {
         mock.reset();
     });
 
-    test('fetchPosts should return data', async () => {
+    test('getPosts should return data', async () => {
         const posts = [{userId: 1, id: 1, title: 'Post 1'}];
         mock.onGet(API_BASE_URL).reply(200, posts);
 
         const result = await getPosts();
         expect(result).toEqual(posts);
+    });
+
+    test('getPostById should return a single post', async () => {
+        const post = {userId: 1, id: 1, title: 'Post 1'}
+        mock.onGet(`${API_BASE_URL}/1`).reply(200, post);
+
+        const result = await getPostById(1);
+        expect(result).toEqual(post);
     });
 });
